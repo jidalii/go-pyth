5bdc3c46763a7a832871508d91f823f255e578f7
Jida Li <77774296+jidalii@users.noreply.github.com>
2025-03-09 14:39:29 -0400

    feat: added discardService and its test

---
+++ app/pyth-handler/rpc/internal/handler/services/discard/discardService.go
@@ -0,0 +1,37 @@
+package discard
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/zeromicro/go-zero/core/logx"
+
+	"pyth-go/app/pyth-common/types"
+	"pyth-go/app/pyth-handler/rpc/internal/svc"
+	"pyth-go/common/zutils/arrayUtils"
+)
+
+type discardMessageService struct {
+	svcCtx *svc.ServiceContext
+}
+
+const discardMessageKey = "discard_msg"
+
+func NewDiscardMessageService(svcCtx *svc.ServiceContext) *discardMessageService {
+	return &discardMessageService{svcCtx: svcCtx}
+}
+
+func (l discardMessageService) IsDiscard(ctx context.Context, taskInfo *types.TaskInfo) bool {
+	discardMessageTemplateIds, err := l.svcCtx.RedisClient.SmembersCtx(ctx, discardMessageKey)
+	if err != nil {
+		logx.Errorw("discardMessageService smembers ", logx.Field("err", err))
+		return false
+	}
+	if len(discardMessageTemplateIds) == 0 {
+		return false
+	}
+	if arrayUtils.ArrayIn(discardMessageTemplateIds, strconv.FormatInt(taskInfo.MessageTemplateId, 10)) {
+		return true
+	}
+	return false
+}
---
+++ app/pyth-handler/rpc/internal/handler/services/discard/discardService_test.go
@@ -0,0 +1,66 @@
+package discard_test
+
+import (
+	"context"
+	"flag"
+	"strconv"
+	"testing"
+    "math/rand"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/zeromicro/go-zero/core/conf"
+
+	"pyth-go/app/pyth-common/types"
+	"pyth-go/app/pyth-handler/rpc/internal/config"
+	"pyth-go/app/pyth-handler/rpc/internal/handler/services/discard"
+	"pyth-go/app/pyth-handler/rpc/internal/svc"
+	"pyth-go/common/zutils/arrayUtils"
+)
+
+var configFile = flag.String("f", "../../../../etc/pythhandler.yaml", "the config file")
+
+const discardMessageKey = "discard_msg"
+
+func addDiscardMessage(ctx context.Context, svcCtx *svc.ServiceContext, keys []string) error {
+	_, err := svcCtx.RedisClient.SaddCtx(ctx, discardMessageKey, keys)
+	return err
+}
+
+func delDiscardMessage(ctx context.Context, svcCtx *svc.ServiceContext, keys []string) error {
+	_, err := svcCtx.RedisClient.SremCtx(ctx, discardMessageKey, keys)
+	return err
+}
+
+func TestDiscardMessageService(t *testing.T) {
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	svcCtx := svc.NewServiceContext(c)
+
+	ctx := context.Background()
+
+	discardMessageService := discard.NewDiscardMessageService(svcCtx)
+
+	discardKeys := []string{"1", "2", "10", "19"}
+	err := delDiscardMessage(ctx, svcCtx, discardKeys)
+	assert.Nil(t, err)
+	err = addDiscardMessage(ctx, svcCtx, discardKeys)
+	assert.Nil(t, err)
+
+	for i := 0; i < 20; i++ {
+		taskInfo := &types.TaskInfo{
+			MessageTemplateId: rand.Int63n(20),
+		}
+
+		res := discardMessageService.IsDiscard(ctx, taskInfo)
+		if arrayUtils.ArrayIn(discardKeys, strconv.FormatInt(taskInfo.MessageTemplateId, 10)) {
+            assert.True(t, res)
+            t.Logf("discarded msg{id %d}", taskInfo.MessageTemplateId)
+		} else {
+			assert.False(t, res)
+            t.Logf("not discarded msg{id %d}", taskInfo.MessageTemplateId)
+		}
+	}
+
+    err = delDiscardMessage(ctx, svcCtx, discardKeys)
+	assert.Nil(t, err)
+}
